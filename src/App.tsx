ed587522f96ee0c21fcb62aa0da129bbacc050f9
Kevin <kevin.tassi@gmail.com>
2023-02-19 22:05:53 +0100

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,65 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as THREE from 'three';
+import { OrbitControls } from '@three-ts/orbit-controls';
+const getCube = () => {
+  const geometry = new THREE.BoxGeometry(5, 1, 1);
+  const material = new THREE.MeshBasicMaterial({ color: 'red' });
+  const cube = new THREE.Mesh(geometry, material);
+
+  return cube
+}
+
+type InitParams = {
+  target: HTMLElement
+}
+const init = ({
+  target
+}: InitParams) => {
+  const scene = new THREE.Scene();
+  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  const renderer = new THREE.WebGLRenderer();
+  const controls = new OrbitControls(camera, renderer.domElement);
+
+  renderer.setSize(500, 500)
+  console.log('append child')
+  target.appendChild(renderer.domElement);
+
+  const animate = () => {
+    requestAnimationFrame(animate);
+    controls.update();
+    renderer.render(scene, camera);
+  }
+
+  return {
+    scene,
+    camera,
+    start: animate,
+    controls
+  }
+}
+
 
 function App() {
+  const a = ''
+
+  useEffect(() => {
+    console.log('effect')
+    const { scene, camera, start } = init({
+      target: document.body
+    })
+
+    const cube = getCube()
+    scene.add(cube);
+    camera.position.z = 100;
+    start()
+
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      cocou
     </div>
   );
 }
